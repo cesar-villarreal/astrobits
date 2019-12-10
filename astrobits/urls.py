d313c6e5b7d36e2cd6c3ec265c18f156bda6e718
cesar-villarreal <csr.astro@gmail.com>
2019-12-10 13:49:42 -0600

    dont know

--- astrobits/urls.py
+++ astrobits/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
 	path('',       include('home.urls')),
     path('admin/', admin.site.urls),
+	path('home/', include('home.urls')),
 	path('stuff/', include('stuff.urls')),
 ]
