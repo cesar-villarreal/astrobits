1981fdef3b55aa253c82eb839478735174030dd2
cesar-villarreal <csr.astro@gmail.com>
2020-01-22 12:39:18 -0600

    Contact form added

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
                path('', views.indexView, name='index'),
                path('home/about', views.aboutView, name='about'),
+               path('home/contact', views.ContactView, name='contact') 
               ]
