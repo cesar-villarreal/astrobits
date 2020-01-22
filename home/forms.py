c42bcb3996be6d740e1583f74662f7d92bc0e285
cesar-villarreal <csr.astro@gmail.com>
2020-01-22 12:39:18 -0600

    Contact form added

--- home/forms.py
+++ home/forms.py
@@ -3,6 +3,14 @@ from django import forms
 
 
 class SendMail(forms.Form):
+	contact_mail = forms.CharField(label="Your E-Mail",
+                                   max_length=30,
+                                   widget=forms.TextInput(
+                                                          attrs={"placeholder": "Write your E-Mail",
+                                                                 "size":31}
+                                                         )
+                                   )
+
 	subject = forms.CharField(label="Subject",
                               max_length=1000,
                               widget=forms.TextInput(
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from .forms import SendMail
 
 def indexView(request):
 	title = "Home"
@@ -7,3 +9,19 @@ def indexView(request):
 def aboutView(request):
 	title = "About"
 	return render(request, 'about.html', {'title': title })
+
+def ContactView(request):
+	title = "Contact"
+
+	if request.method == "POST":
+		send_mail(request.POST.get('subject'),
+                  request.POST.get('message'),
+                  "astro@cvillarreal.xyz",
+                  ["csr.astro@gmail.com"])	
+
+		return render(request, 'contact.html', {'title': title,
+                                                'SendMail': SendMail,
+                                                'email_success': 'Your E-mail has been sent!'})
+	if request.method == "GET":
+		return render(request, 'contact.html', {'title': title,
+                                                'SendMail': SendMail})
