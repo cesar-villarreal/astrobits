372924c43b703568feefc62d0ae02e9f520dc388
cesar-villarreal <csr.astro@gmail.com>
2020-01-25 15:10:15 -0600

    Updating the Download page without restart the servers fixed. Still to improve

--- stuff/views.py
+++ stuff/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 #from django.http import HttpResponse
 
 def AstroView(request):
@@ -6,10 +8,18 @@ def AstroView(request):
 	return render(request, 'astro.html', {'title': title, })
 
 def DownloadView(request):
-	from stuff.files_list import GetFilesList
+	url = "http://cvillarreal.xyz/astrobits/stuff/download"
+	response_text = requests.get(url, params={}).text
+	links_delete = 5
+	soup = BeautifulSoup(response_text, 'html.parser')
+	a = soup.find_all('a')
+	files_list = [url+'/'+i.get('href') for i in a]
+	for i in range(links_delete):
+		del files_list[0]
+
 	title = "Download"
 	return render(request, 'download.html', {'title': title,
-                                             'files_list': GetFilesList})
+                                                'files_list': files_list})
 
 def VimView(request):
 	title = "VIM"
